49957147fb0f4727c8b6e6faea76945e84268688
shekinah <nyamburamary1212@gmail.com>
2023-11-22 12:38:47 +0300

    Complete conditional statements task

--- lib/control_flow.py
+++ lib/control_flow.py
@@ -2,16 +2,50 @@
 
 def admin_login(username, password):
     # your code here
-    pass
+    user_name = username.lower()
+    if user_name == "admin" and password == "12345":
+        return"Access granted"
+    else:
+        return"Access denied"
 
 def hows_the_weather(temperature):
     # your code here
-    pass
+    if temperature > 85:
+        return("It's too dang hot out there!")
+    elif temperature >=40 and temperature <=65:
+        return("It's a little chilly out there!")
+    elif temperature <40:
+        return("It's brisk out there!")
+    else:
+        return("It's perfect out there!")
+    
 
 def fizzbuzz(num):
     # your code here
-    pass
+    if num%3 == 0 and num%5 == 0:
+        return("FizzBuzz")
+    elif num%3 == 0:
+        return("Fizz")
+    elif num%5 == 0:
+        return("Buzz")
+    else:
+        return(num)
+    
 
 def calculator(operation, num1, num2):
     # your code here
-    pass
+    if operation == "+":
+        return num1 + num2
+    elif operation == "-":
+        return num1 - num2
+    elif operation == "*":
+        return num1 * num2
+    elif operation == "/":
+        if num2 != 0:
+          return num1 /num2
+        else:
+            return"Division by zero is not allowed!"
+    else:
+         print("Invalid operation!")
+         return None
+            
